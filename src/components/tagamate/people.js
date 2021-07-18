40ecc5358782d0257e8ac0684849978b0e56f14a
EC2 Default User <ec2-user@ip-172-31-33-116.ec2.internal>
2021-07-18 01:43:19 +0000

    7.18 update

--- src/components/tagamate/people.js
+++ src/components/tagamate/people.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from 'styled-components';
+import { Link } from 'gatsby';
 import {Grid } from '@material-ui/core';
 import {SSection, SSectionTitle,SCredit, SSectionDiscription} from '../common/common.js'
 import Img1 from '../../images/index/people/yago.jpg'
@@ -14,6 +15,109 @@ import Img9 from '../../images/index/people/watabe.jpg'
 import Img10 from '../../images/index/people/murata.jpg'
 import Img11 from '../../images/index/people/watarai.jpg'
 import Img12 from '../../images/index/people/hosokawa.jpg'
+
+const Tagamates = [
+    {
+        img:Img1,
+        title:'田瓶市長',
+        message:'田瓶史の案内人',
+        name:'矢後 源五郎',
+        roman:'Gengoroh Yago'
+    },
+    {
+        img:Img2,
+        title:'沼田ホテル女将',
+        message:'最高のもてなしを',
+        name:'餌取 京子',
+        roman:'Kyoko Etori'
+    },
+    {
+        img:Img3,
+        title:'村田工業 都市開発担当',
+        message:'安全/快適な街造り',
+        name:'橋本 道夫',
+        roman:'Michio Hashimoto'
+    },
+    {
+        img:Img4,
+        title:'「椿の会」会長',
+        message:'シニアの力の集結',
+        name:'木村 茂',
+        roman:'Shigeru Kimura'
+    },
+    {
+        img:Img5,
+        title:'政治家・実業家',
+        message:'田瓶市政の風雲児',
+        name:'大泉 薔薇彦',
+        roman:'Barahiko Ohizumi'
+    },
+    {
+        img:Img6,
+        title:'田瓶競馬場　調教師',
+        message:'動物と生きる',
+        name:'谷川 加奈子',
+        roman:'Kanako Tanigawa'
+    },
+    {
+        img:Img7,
+        title:'「おかよたか」生産者',
+        message:'常に新たな発見を',
+        name:'飯田 かなえ',
+        roman:'Kanae Iida'
+    },
+    {
+        img:Img8,
+        title:'毛総新聞　編集部',
+        message:'活字に命を吹き込む',
+        name:'浦川 麗子',
+        roman:'Reiko Urakawa'
+    },
+    {
+        img:Img9,
+        title:'ベンチャー起業家',
+        message:'アイデアを形に',
+        name:'渡部 学',
+        roman:'Manabu Watabe'
+    },
+    {
+        img:Img10,
+        title:'山野辺総合病院医師',
+        message:'対話から生まれる信頼',
+        name:'村田 桔平',
+        roman:'Kippei Murata'
+    },
+    {
+        img:Img11,
+        title:'小説家、評論家',
+        message:'動画より鮮やかに',
+        name:'渡会 理砂',
+        roman:'Risa Watarai'
+    },
+    {
+        img:Img12,
+        title:'フォーリッジ事業統括',
+        message:'人工知能の活用',
+        name:'マイケル　岸田',
+        roman:'Michael Kishida'
+    }
+    ]
+
+const Tagamate = ({img, title, message, name, roman})=>{
+    console.log(img)
+    return(
+        <Grid item xs={12} sm={6} lg={4}>
+            <Link to={'/tagamate'+`/${roman.split(' ').pop()}/`}>
+                <SPeopleWrap>
+                    <img src={img} alt={roman}/>
+                        <div>{title}</div>
+                        <p>{message}</p>
+                        <span>{name}</span><i>{roman}</i>
+                </SPeopleWrap>
+            </Link>
+        </Grid>
+    )
+} 
 
 const People= () => {
     return (
@@ -35,102 +139,11 @@ const People= () => {
                         夢見る神の子が作る街
                     </SSectionDiscription>
                     <Grid container>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img1} alt="yago"/>
-                                <div>田瓶市長</div>
-                                <p>田瓶史の案内人</p>
-                                <span>矢後 源五郎</span><i>Gengoroh Yago</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img2} alt="etori"/>
-                                <div>沼田ホテル 4代目女将</div>
-                                <p>最高のもてなしを</p>
-                                <span>餌取 京子</span><i>Kyoko Etori</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img3} alt="hashimoto"/>
-                                <div>村田工業 都市開発担当</div>
-                                <p>安全/快適な街造り</p>
-                                <span>橋本 道夫</span><i>Michio Hashimoto</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img4} alt="kimura"/>
-                                <div>「椿の会」会長</div>
-                                <p>シニアの力の集結</p>
-                                <span>木村 茂</span><i>Shigeru Kimura</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img5} alt="kimura"/>
-                                <div>政治家・実業家</div>
-                                <p>田瓶市政の風雲児</p>
-                                <span>大泉 薔薇彦</span><div/><i>Barahiko Ohizumi</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img6} alt="tanigawa"/>
-                                <div>田瓶競馬場　調教師</div>
-                                <p>動物と生きる</p>
-                                <span>谷川 加奈子</span><i>Kanako Tanigawa</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img7} alt="iida"/>
-                                <div>コメ農家</div>
-                                <p>常に新たな発見を</p>
-                                <span>飯田 かおり</span><i>Kaori Iida</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img8} alt="urakawa"/>
-                                <div>毛総新聞　編集部</div>
-                                <p>活字に命を吹き込む</p>
-                                <span>浦川 麗子</span><i>Reiko Urakawa</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img9} alt="watabe"/>
-                                <div>航空エンジニア</div>
-                                <p>アイデアを形に</p>
-                                <span>渡部 学</span><i>Manabu Watabe</i>
-                            </SPeopleWrap>
-                        </Grid>
-                                                <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img10} alt="watabe"/>
-                                <div>山野辺総合病院医師</div>
-                                <p>対話から生まれる信頼</p>
-                                <span>村田 桔平</span><i>Kippei Murata</i>
-                            </SPeopleWrap>
-                        </Grid>
-                        <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img11} alt="watabe"/>
-                                <div>小説家、評論家</div>
-                                <p>今を切り取る文章</p>
-                                <span>渡会 理砂</span><i>Risa Watarai</i>
-                            </SPeopleWrap>
-                        </Grid>
-                                                <Grid item xs={12} sm={6} lg={4}>
-                            <SPeopleWrap>
-                                <img src={Img12} alt="watabe"/>
-                                <div>データサイエンティスト</div>
-                                <p>人工知能の活用</p>
-                                <span>マイケル　岸田</span><i>Michael Kishida</i>
-                            </SPeopleWrap>
-                        </Grid>
+                        {Tagamates.map((item)=>{
+                            return(
+                                <Tagamate key={item.roman} img={item.img} title={item.title} message={item.message} name={item.name} roman={item.roman}/>
+                            )
+                        })}
                     </Grid>
                 </Grid>
 
